027fa65a4d7618c83cb54fd915e488d6cf01a2a2
125753 <dheeraj1@ecomexpress.in>
2023-12-25 19:33:35 +0530

    Saving image and using jpa commonly used annotations

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,11 +1,14 @@
 package org.example;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Student {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int rollNumber;
     private String name;
     private String city;
